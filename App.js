c46eb2d50cf10299148dcff0f45b17ffe7afb81c
SHUBHAM MOGHA <shubham.mogha@innovationm.com>
2023-07-06 15:33:32 +0530

    React Demo

--- App.js
+++ App.js
@@ -21,28 +21,24 @@
 
 // How to create this structure in React JS
 
-// const parent = React.createElement("div", { id: "parent" }, [
-//   React.createElement("div", { id: "child1" }, [
-//     React.createElement("h1", {}, "I am H1 tag"),
-//     React.createElement("h2", {}, "I am H2 tag"),
-//   ]),
-//   React.createElement("div", { id: "child2" }, [
-//     React.createElement("h1", {}, "I am H1 tag"),
-//     React.createElement("h2", {}, "I am H2 tag"),
-//   ])
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am H1 tag"),
+    React.createElement("h2", {}, "I am H2 tag"),
+  ]),
+]);
 
-// ]);
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello World using React JS"
+);
 
-//   const heading = React.createElement(
-//     "h1",
-//     {id:"heading"},
-//     "Hello World using React JS"
-//   );
-// This is called JSX
+//This is called JSX
 
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
-
-
-//-----------------------------------------------//
-// React Elements
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
