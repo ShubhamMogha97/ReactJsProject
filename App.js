fa55ec5aad51e568122a4e6c265b0d150b274b85
SHUBHAM MOGHA <shubham.mogha@innovationm.com>
2023-07-06 15:36:20 +0530

    React Mesages

--- App.js
+++ App.js
@@ -28,7 +28,7 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am H1 tag"),
-    React.createElement("h2", {}, "I am H2 tag"),
+    React.createElement("h2", {}, "I am H2 tags"),
   ]),
 ]);
 
